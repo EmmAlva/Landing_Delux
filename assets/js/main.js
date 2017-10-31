a8806d2393cddfa69444d70281151c05dcb6df1f
EmmAlva <clioemma_28@hotmail.com>
2017-10-31 14:47:53 -0500

    html_css

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$('.carousel').carousel()
+var search = document.getElementById("search-clic");
+// var spotlight = document.getElementById("search-spotlight");
+var carouselImg = document.getElementsByClassName("carousel-img");
+// var carouselImg = document.getElementsByClassName("carousel-img")[1];
+// var carouselImg = document.getElementsByClassName("carousel-img")[2];
+
+search.addEventListener("click", function(){
+    alert("hola");
+    for(var i =0; i<carouselImg.length;i++){
+        carouselImg[i].classList.add("in-dark");
+    }
+    // spotlight.classList.remove("d-none");
+    
+});
+
