9d1e6cd996be84d60ff2c9d9654562a09b15ac12
EmmAlva <clioemma_28@hotmail.com>
2017-11-05 20:34:54 -0500

    add styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,14 @@
 $('.carousel').carousel()
 var search = document.getElementById("search");
-// var spotlight = document.getElementById("search-spotlight");
+var spotlight = document.getElementById("search-spotlight");
 var carouselImg = document.getElementsByClassName("carousel-img");
-// var carouselImg = document.getElementsByClassName("carousel-img")[1];
-// var carouselImg = document.getElementsByClassName("carousel-img")[2];
+var carouselImg = document.getElementsByClassName("carousel-img")[1];
+var carouselImg = document.getElementsByClassName("carousel-img")[2];
 
 search.addEventListener("click", function(){
-    alert("hola");
     for(var i =0; i<carouselImg.length;i++){
         carouselImg[i].classList.add("in-dark");
     }
-    // spotlight.classList.remove("d-none");
-    
+    spotlight.classList.remove("d-none"); 
 });
 
