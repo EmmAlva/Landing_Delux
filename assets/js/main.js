b63e7c91cd9d744c58cd93e21ffcac7cd3477152
EmmAlva <clioemma_28@hotmail.com>
2017-11-06 10:28:31 -0500

    btn back top

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,16 @@ search.addEventListener("click", function(){
     spotlight.classList.remove("d-none"); 
 });
 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('#scroll').fadeIn('slow');
+        }else{
+            $('#scroll').fadeOut('slow');
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
