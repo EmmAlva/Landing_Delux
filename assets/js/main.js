adab269db98e909c552148472a787a9607c27977
EmmAlva <clioemma_28@hotmail.com>
2017-11-07 01:06:43 -0500

    change icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,5 @@
 $('.carousel').carousel()
-var search = document.getElementById("search");
-var spotlight = document.getElementById("search-spotlight");
-var carouselImg = document.getElementsByClassName("carousel-img");
-var carouselImg = document.getElementsByClassName("carousel-img")[1];
-var carouselImg = document.getElementsByClassName("carousel-img")[2];
 
-search.addEventListener("click", function(){
-    for(var i =0; i<carouselImg.length;i++){
-        carouselImg[i].classList.add("in-dark");
-    }
-    spotlight.classList.remove("d-none"); 
-});
 
 $(document).ready(function(){
     $(window).scroll(function(){
